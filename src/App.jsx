589f644084f9273b9727cbb1654c045dfeab19d1
faqihfnf <faqih.fnf@gmail.com>
2025-01-25 12:33:33 +0700

    update header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Search from "./components/Search";
+import Header from "./components/Header";
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -7,12 +8,7 @@ export default function App() {
     <main>
       <div className="pattern">
         <div className="wrapper">
-          <header>
-            <img src="./hero.png" alt="hero-banner" />
-            <h1>
-              Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
-            </h1>
-          </header>
+          <Header />
           <Search seacrhTerm={searchTerm} setSearchTerm={setSearchTerm} />
           <h1 className="text-white">{searchTerm}</h1>
         </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="./hero.png" alt="hero-banner" />
+      <h1>
+        Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle
+      </h1>
+    </header>
+  );
+}
